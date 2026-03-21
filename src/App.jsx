c69d8d4b1bbe996a2fcb587c5fda3e85c54e9519
anjishnum <anjishnumahanta@gmail.com>
2026-03-21 15:40:15 +0530

    Initial commit: barebones react + tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-gray-50">
+      <h1 className="text-4xl font-bold text-gray-900 border-b-2 border-gray-900 pb-2">
+        Restaurantza
+      </h1>
+    </div>
+  )
+}
